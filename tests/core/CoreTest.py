8733b0e0177b7086200989c215a6e17c298ebdcc
Makman2 <makman@alice.de>
2016-12-07 01:21:52 +0100

    WIP - tests

--- tests/core/CoreTest.py
+++ tests/core/CoreTest.py
@@ -13,27 +13,27 @@ class BearB(Bear):
     DEPENDENCIES = set()
 
 
-class BearC(Bear):
+class BearC_NeedsB(Bear):
     DEPENDENCIES = {BearB}
 
 
-class BearD(Bear):
-    DEPENDENCIES = {BearC}
+class BearD_NeedsC(Bear):
+    DEPENDENCIES = {BearC_NeedsB}
 
 
-class BearE(Bear):
-    DEPENDENCIES = {BearA, BearD}
+class BearE_NeedsAD(Bear):
+    DEPENDENCIES = {BearA, BearD_NeedsC}
 
 
 class CoreTest(unittest.TestCase):
-    def test_initialize_dependencies(self):
-        bear_e = BearE(Section('test-section'))
+    def test_initialize_dependencies1(self):
+        bear_e = BearE_NeedsAD(Section('test-section'))
         dependency_tracker = initialize_dependencies({bear_e})
 
         self.assertEqual(len(dependency_tracker.get_dependencies(bear_e)), 2)
         self.assertTrue(any(isinstance(bear, BearA) for bear in
                             dependency_tracker.get_dependencies(bear_e)))
-        self.assertTrue(any(isinstance(bear, BearD) for bear in
+        self.assertTrue(any(isinstance(bear, BearD_NeedsC) for bear in
                             dependency_tracker.get_dependencies(bear_e)))
 
         bear_a = next(
@@ -41,7 +41,7 @@ class CoreTest(unittest.TestCase):
             if isinstance(bear, BearA))
         bear_d = next(
             bear for bear in dependency_tracker.get_dependencies(bear_e)
-            if isinstance(bear, BearD))
+            if isinstance(bear, BearD_NeedsC))
 
         self.assertIs(bear_a.section, bear_e.section)
         self.assertIs(bear_d.section, bear_e.section)
@@ -51,7 +51,7 @@ class CoreTest(unittest.TestCase):
         self.assertEqual(len(dependency_tracker.get_dependencies(bear_d)), 1)
         bear_c = dependency_tracker.get_dependencies(bear_d).pop()
         self.assertIs(bear_c.section, bear_e.section)
-        self.assertIsInstance(bear_c, BearC)
+        self.assertIsInstance(bear_c, BearC_NeedsB)
 
         self.assertEqual(len(dependency_tracker.get_dependencies(bear_c)), 1)
         bear_b = dependency_tracker.get_dependencies(bear_c).pop()
@@ -60,6 +60,42 @@ class CoreTest(unittest.TestCase):
 
         self.assertEqual(dependency_tracker.get_dependencies(bear_b), set())
 
-        # TODO Test if we preinstantiate some bears, all bears, some bears twice
-        # TODO And the case when no dependencies at all occur! Test multi-section
-        # TODO   setup, with and without dependencies.
+    def test_initialize_dependencies2(self):
+        section = Section('test-section')
+        dependency_tracker = initialize_dependencies(
+            {BearA(section), BearB(section)})
+
+        self.assertTrue(dependency_tracker.all_dependencies_resolved)
+
+    def test_initialize_dependencies3(self):
+        # Test whether pre-instantiated dependency bears are correctly
+        # (re)used.
+        section = Section('test-section')
+        bear_b = BearB(section)
+        bear_c = BearC_NeedsB(section)
+        dependency_tracker = initialize_dependencies({bear_b, bear_c})
+
+        self.assertEqual(dependency_tracker.get_all_dependants(), {bear_c})
+        self.assertEqual(dependency_tracker.get_dependencies(bear_c), {bear_b})
+
+    def test_initialize_dependencies4(self):
+        # Test whether pre-instantiated bears which belong to different
+        # sections are not (re)used, as the sections are different.
+        section1 = Section('test-section1')
+        section2 = Section('test-section2')
+
+        bear_b = BearB(section1)
+        bear_c = BearC_NeedsB(section2)
+        dependency_tracker = initialize_dependencies({bear_b, bear_c})
+
+        self.assertEqual(dependency_tracker.get_all_dependants(), {bear_c})
+        dependencies = dependency_tracker.get_all_dependencies()
+        self.assertEqual(len(dependencies), 1)
+        dependency = dependencies.pop()
+        self.assertIsInstance(dependency, BearB)
+        self.assertIsNot(dependency, bear_b)
+
+        # TODO Test if we preinstantiate some bears, all bears, some bears
+        # TODO   twice
+        # TODO And the case when no dependencies at all occur! Test
+        # TODO   multi-section setup, with and without dependencies.
